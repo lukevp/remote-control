b035f61f8cb8404c365c408cc67edcf8ef9afa41
CandL Development <hello@candldev.com>
2021-09-18 10:36:42 -0700

    Add electron-forge for installer support

--- desktop-client/main.js
+++ desktop-client/main.js
@@ -1,9 +1,13 @@
+const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
@@ -15,4 +19,12 @@ app.whenReady().then(() => {
 
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
